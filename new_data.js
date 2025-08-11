fc5c0d7d586062ba1049399d8c760c8aaf9f11f3
Omar Elsayed <3mar153153@gmail.com>
2025-08-12 01:45:07 +0300

    Add files via upload

---
+++ new_data.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "Tamally Maak",
+    path: "musics/Tamally Maak1.mp3",
+    artist: "Amr Diab",
+    cover: "images/AmrDiab1.jpg",
+  },
+  {
+    name: "Dehket",
+    path: "musics/Dehket2.mp3",
+    artist: "Amr Diab",
+    cover: "images/AmrDiab2.jpg",
+  },
+  {
+    name: "Lealy Nahary",
+    path: "musics/Lealy Nahary3.mp3",
+    artist: "Amr Diab",
+    cover: "images/AmrDiab3.jpg",
+  },
+  {
+    name: "Helwa ElBedayat",
+    path: "musics/Helwa El_Bedayat4.mp3",
+    artist: "Amr Diab",
+    cover: "images/AmrDiab4.jpg",
+  },
+  {
+    name: "Mayal",
+    path: "musics/diab14.mp3",
+    artist: "Amr Diab",
+    cover: "images/AmrDiab5.jpg",
+  },
+  // {
+  //   name: "Khatfoony ",
+  //   path: "musics/khtfwny.mp3",
+  //   artist: "Amr Diab",
+  //   cover: "images/AmrDiab6.jpg",
+  // },
+];
